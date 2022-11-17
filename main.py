bc8c9217663145124ca04fa33feb1c884d90c03f
KC Chester Chester <113367258+kayceeh24@users.noreply.github.com>
2022-11-17 14:40:07 +0000

    print to return

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from math import pi
 # Function for calculating a surface area for a sphere with user specified radius 
 def calculate_surface_area(radius):
     surface_area = 4 * pi * radius ** 2
-    print(surface_area)
+    return(surface_area)
 
 # Function for calculating a volume for a sphere with user specified radius 
 def calculate_volume(radius):
     volume = 4 / 3 * pi * radius ** 3
-    print(volume)
+    return(volume)
